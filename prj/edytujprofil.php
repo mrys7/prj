0afa9cb6bbb02fffc38256641c25d8818e741364
mrys7 <79912431+mrys7@users.noreply.github.com>
2022-04-04 19:46:55 +0200

    Delete edytujprofil.php

--- prj/edytujprofil.php
+++
@@ -1,9 +0,0 @@
-<div class="editprofile">
-           <form action="changeprofile.php" method="post">
-				<input type="text" class="input-text" name="username" placeholder="Nazwa użytkownika" id="username" required>
-				<input type="password" name="password" placeholder="Hasło" id="password" required>
-				<input type="password" name="password2" placeholder="Powtórz hasło" id="password2" required>
-				<input type="email" name="email" placeholder="email" id="email" required>
-				<input type="submit" value="Wyślij" class="add-set-btn2">
-			</form>
-</div>
